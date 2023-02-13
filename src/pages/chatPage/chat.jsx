36919e4b459b4adce7226193147553373707c7cc
Mohamed Abdifitah Ahmed <learntecnology30@gmail.com>
2023-02-13 15:01:33 +0300

    icon references

--- src/pages/chatPage/chat.jsx
+++ src/pages/chatPage/chat.jsx
@@ -3,12 +3,14 @@ import ChatPageBody from "./body/body";
 import Header from "./header/header";
 import TextField from '@mui/material/TextField';
 import "./index.css";
+import Footer from "./footer/footer";
 const ChatPage = () => {
   return (
     <div className="chat">
         <Header />
         
         <ChatPageBody />
+      
     </div>
   )
 }
