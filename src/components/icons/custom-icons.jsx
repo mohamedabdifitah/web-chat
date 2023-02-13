d04b903e5c16123364f785ad0f87e42f6052de59
Mohamed Abdifitah Ahmed <learntecnology30@gmail.com>
2023-02-13 15:29:59 +0300

    customIcon component

---
+++ src/components/icons/custom-icons.jsx
@@ -0,0 +1,14 @@
+
+export const CustomIcon = (props) => {
+  const {style,name} = props;
+  switch (name) {
+    case 'pin':
+      return (
+      <svg  height="15" width="15" preserveAspectRatio="xMidYMid meet" ><path fill="currentColor" class="grey" d="M12.074 4.21 8.7 8.232l.116 4.233a.4.4 0 0 1-.657.318L.43 6.297a.4.4 0 0 1 .199-.702l4.196-.622L8.196.957a.63.63 0 0 1 .887-.078l2.914 2.445a.63.63 0 0 1 .077.887ZM1.294 14.229a.713.713 0 0 1-1.09-.915l2.674-3.64 1.536 1.288-3.12 3.267Z"></path></svg>     
+      )
+    case 'dropdown':
+      return (
+        <svg style={style} viewBox="0 0 18 18" height="18" width="18" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 18 18" xml:space="preserve"><path fill="currentColor" d="M3.3,4.6L9,10.3l5.7-5.7l1.6,1.6L9,13.4L1.7,6.2L3.3,4.6z"></path></svg>
+      )
+  }
+}
--- src/pages/chatsLists/body/components/chat-card.jsx
+++ src/pages/chatsLists/body/components/chat-card.jsx
@@ -3,6 +3,7 @@ import ChatAvatar from "./avatar";
 import { RiArrowDropDownLine } from "react-icons/ri"
 import Badge from '@mui/material/Badge';
 import CustomBadge  from "../../../../components/badge/badge"
+import { CustomIcon } from "../../../../components/icons/custom-icons";
 const ChatCard = () => {
   return (
     <div className="outline-card">
@@ -18,14 +19,20 @@ const ChatCard = () => {
         <div className="info">
           <span dir="ltr" className="recent-msg text off">+252 61 9857867 changed to +256 775 099136</span>
           <div className="chat-icons">
-            <img src={"/pin.svg"} alt="pin icon" className="chat-icon" />
+            <span className="chat-icon">
+              <CustomIcon name={"pin"}/>
+              
+            </span>
+            {/* <img src={"/pin.svg"} alt="pin icon" className="chat-icon" /> */}
             {/* <Badge badgeContent={4} color="secondary">
               
             </Badge> */}
             <CustomBadge count={1} className={"badge-primary"} />
             {/* <span class="MuiBadge-root MuiBadge-root css-1c32n2y-MuiBadge-root"><span class="MuiBadge-badge MuiBadge-standard MuiBadge-anchorOriginTopRight MuiBadge-anchorOriginTopRightRectangular MuiBadge-overlapRectangular MuiBadge-colorSecondary MuiBadge-badge css-jcn4dz-MuiBadge-badge">4</span></span> */}
-    
-            <img src={"/dropdown.svg"} alt="dropdown" className="chat-icon" />
+            <span className="chat-icon">
+              <CustomIcon name={"dropdown"}/>
+            </span>
+            {/* <img src={"/dropdown.svg"} alt="dropdown" className="chat-icon" /> */}
 
           </div>
         </div>
